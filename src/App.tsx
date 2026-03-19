92e648600dfd0dc3915a5b692e5d5fbec528658a
hellomayth <hello.mayth@gmail.com>
2026-03-19 18:25:13 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -281,7 +281,6 @@ const PORTFOLIO_ITEMS = [
         <p>
           I am incredibly proud of what we have achieved together. The technical challenges were immense, but the personal bonds we formed were even greater. To my colleagues, my mentors, and my stakeholders: thank you for being part of this incredible chapter.
         </p>
-        
       </div>
     )
   },
